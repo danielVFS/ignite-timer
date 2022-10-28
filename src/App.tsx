d73469956d39e668674eeae2327017fb7ae532a0
danielVFS <danielv759153@gmail.com>
2022-10-28 18:44:34 -0300

    style: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+    </ThemeProvider>
   );
 }
