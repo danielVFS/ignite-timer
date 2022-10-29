26285fa3bbfbbc19ab162012f7d19e6e13c5474c
danielVFS <danielv759153@gmail.com>
2022-10-29 15:23:09 -0300

    feat(home): basic structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react'
+import { HomeContainer } from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form>
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">Durant</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} /> Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
